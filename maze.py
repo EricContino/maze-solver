2cc78623c80891b6401249b834f402a1289d9c54
EricContino <evcontino5107@gmail.com>
2025-06-08 16:13:41 -0400

    solved the maze

--- maze.py
+++ maze.py
@@ -121,3 +121,43 @@ class Maze:
             for cell in col:
                 cell.visited = False
 
+    def solve(self):
+        return self.__solve_r(0,0)
+
+    def __solve_r(self, i, j):
+        self.__animate()
+        self.__cells[i][j].visited = True
+
+        if i == self.__num_cols - 1 and j == self.__num_rows - 1:
+            return True
+        
+        # left
+        if i > 0 and self.__cells[i][j].has_left_wall is False and self.__cells[i - 1][j].visited is False:
+            self.__cells[i][j].draw_move(self.__cells[i - 1][j])
+            if self.__solve_r(i - 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i - 1][j], True)
+        # right
+        if i < self.__num_cols and self.__cells[i][j].has_right_wall is False and self.__cells[i + 1][j].visited is False:
+            self.__cells[i][j].draw_move(self.__cells[i + 1][j])
+            if self.__solve_r(i + 1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i + 1][j], True)
+        # bottom
+        if j < self.__num_rows and self.__cells[i][j].has_bottom_wall is False and self.__cells[i][j + 1].visited is False:
+            self.__cells[i][j].draw_move(self.__cells[i][j + 1])
+            if self.__solve_r(i, j + 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j + 1], True)
+        # top
+        if j > 0 and self.__cells[i][j].has_top_wall is False and self.__cells[i][j - 1].visited is False:
+            self.__cells[i][j].draw_move(self.__cells[i][j - 1])
+            if self.__solve_r(i, j - 1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j - 1], True)
+
+
